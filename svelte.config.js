ebe58017e541322687232d8b53b44b097e85d91c
bufgix <farukoruc.dev@gmail.com>
2022-03-15 16:22:15 +0300

    fix: change sveltekit addapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 import relativeImages from 'mdsvex-relative-images';
