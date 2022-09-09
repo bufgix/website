12d81b90f3c4e58884e5a632038276e2922e1fc2
bufgix <farukoruc.dev@gmail.com>
2022-09-10 01:09:30 +0300

    build: sync bookmarks action

--- scripts/fetchBookmarks.js
+++ scripts/fetchBookmarks.js
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import path from 'path';
 import fetch from 'node-fetch';
+import MD5 from 'crypto-js/md5.js';
 
 const hashFile = path.join(process.cwd(), 'hash');
 
@@ -13,8 +14,15 @@ async function fetchBookmarks() {
 		})
 	).json();
 
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	console.log('Result:', bookmarks.result);
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	bookmarks.items.forEach((b) => console.log(b.link));
+
 	const oldHash = fs.existsSync(hashFile) ? fs.readFileSync(hashFile, 'utf8') : '';
-	const newHash = Buffer.from(JSON.stringify(bookmarks)).toString('base64');
+	const newHash = MD5(JSON.stringify(bookmarks)).toString();
 	if (oldHash === newHash) {
 		console.log('No changes! Current hash:', newHash);
 		process.exit(1);
