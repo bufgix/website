b26fc37254cc3ab15ecdfcd5c490fb8d187e687c
bufgix <farukoruc.dev@gmail.com>
2022-09-21 19:10:20 +0300

    refactor: change shit Google Analytics to umami

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -1,28 +1,7 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-
-	$: {
-		if (typeof gtag !== 'undefined') {
-			// eslint-disable-next-line no-undef
-			gtag('config', 'G-Q8C4QGK86X', {
-				page_title: document.title,
-				page_path: $page.url.pathname
-			});
-		}
-	}
-</script>
-
 <svelte:head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q8C4QGK86X"></script>
-
-	<script>
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-
-		gtag('js', new Date());
-		gtag('config', 'G-Q8C4QGK86X');
-	</script>
+	<script
+		async
+		defer
+		data-website-id="afd5bda2-2537-4822-8ef8-2ce95742a102"
+		src="https://collector-oylgdtcxp-bufgix.vercel.app/umami.js"></script>
 </svelte:head>
