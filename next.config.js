eeca872f7711402a89139c9725a0e0679d51af3c
bufgix <farukoruc.dev@gmail.com>
2024-06-22 14:23:13 +0300

    chore: Add .yarnrc.yml, update .gitignore, next.config.js, and index.mdx

--- next.config.js
+++ next.config.js
@@ -3,4 +3,16 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.jsx',
 });
 
-module.exports = withNextra();
+module.exports = {
+  ...withNextra(),
+  async redirects() {
+    return [
+      {
+        source: '/cv',
+        destination: 'https://docs.google.com/document/d/1tN6QMFZBXqTrzR9U2Gfjbctsv6FWRjSCq3qcAZWMQ74/edit?usp=sharing',
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
+};
